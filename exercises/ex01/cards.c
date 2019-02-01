b4b5c7a844e280f6dba2b0fe60fe7b722a569c80
ASHSWAN1999 <ashley.swanson@students.olin.edu>
2019-02-01 16:58:23 -0500

    Revised refactoring

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,12 +1,22 @@
 /*
-  @author Ashley Swanson
+@author Ashley Swanson
 
-  Card counting program
+Card counting program.
 */
-
 
 #include <stdio.h>
 #include <stdlib.h>
+
+/*Prompts the user to enter a card and updates the contents
+  of card name.
+
+  card_name: pointer to array where most recent card is stored
+*/
+void get_input(char* card_name) {
+  puts("Enter the card: ");
+  scanf("%2s", card_name);
+}
+
 
 /*Tests the value of the input and determines if the count
   should be incremented, decremented, or remain constant.
@@ -14,56 +24,50 @@
   count: keeps track of high and low cards drawn
   val: numeric value of most recently drawn card
 */
- int update_counter(int count, int val) {
-   if((val>2)&&(val<7)) {
-     count++;
-   } else if(val==10) {
-     count--;
-   }
-   return count;
- }
-
-/*Correlates face cards with numerical values and typecasts
-  numerical values to integers. Throws warning if input is invalid.
-
-  card_name: first two characters of string entered by user.
-*/
-void get_val() {
-  char card_name[3];
-  int count = 0;
-  do {
-    int val = 0;
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-     switch(card_name[0]) {
-       case 'K':
-       case 'Q':
-       case 'J':
-         val = 10;
-         break;
-       case 'A':
-         val = 11;
-         break;
-       case 'X':
-         continue;
-       default:
-         val = atoi(card_name);
-         if((val<1)||(val>10)) {
-           puts("I don't understand that value!");
-           continue;
-         }
-      }
-      count = update_counter(count, val);
-      printf("%i\n", count);
-  } while (card_name[0]!='X');
+int get_value(int count, int val) {
+  if ((val>2)&&(val<7)) {
+    count++;
+  } if (val==10) {
+    count--;
+  }
+  return count;
 }
 
- /*Calls function to prompts user for input and prints the
-  resulting count. Continues until the user enters 'X'
+/*Asks user for playing card and prints a counter indicating
+  how many high or low cards have been drawn. Loops until
+  user enters 'X'
 
-   User input is truncated to two characters.
- */
- int main() {
-   get_val();
-   return 0;
- }
+  User input is truncated to two characters
+*/
+int main() {
+  int count = 0;
+  int val = 0;
+  char card_name[3];
+
+  do {
+    get_input(card_name);
+
+    switch(card_name[0]) {
+      case 'K':
+      case 'Q':
+      case 'J':
+        val = 10;
+        break;
+      case 'A':
+        val = 11;
+        break;
+      case 'X':
+        continue;
+      default:
+        val = atoi(card_name);
+        if ((val<1) || (val>10)) {
+          puts("I don't understnad that value!");
+          continue;
+        }
+    }
+    printf("Input: %c  Val: %i\n", card_name[0], val);
+    count = get_value(count, val);
+    printf("%i\n", count);
+
+  } while (card_name[0]!='X');
+}
